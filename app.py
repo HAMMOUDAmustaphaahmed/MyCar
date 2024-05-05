fda794f65e23fd52b31691bccb6b3fcef5095035
HAMMOUDAmustaphaahmed <hammouda.ahmedmustapha@gmail.com>
2024-05-05 20:46:05 +0100

    version 2.0

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from controllers import blueprint
 from models import init_app
 
 app = Flask(__name__)
+# Set the JWT secret key
+app.config['JWT_SECRET_KEY'] = 'password'
+
+
 init_app(app)
 
+jwt = JWTManager(app)
 app.register_blueprint(blueprint)
 
 if __name__ == '__main__':
--- controllers.py
+++ controllers.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from models import User, Car
+from models import db, User, Car
 
 blueprint = Blueprint('controllers', __name__)
 
@@ -22,6 +22,7 @@ def login():
         if user and user.password == password:
             access_token = create_access_token(identity=user.id)
             return jsonify(access_token=access_token)
+            
         else:
             return jsonify({'msg': 'Bad username or password'}), 401
     else:
@@ -47,3 +48,20 @@ def user():
     user = User.query.get(user_id)
     cars = Car.query.filter_by(user_id=user_id).all()
     return jsonify({"user": {"username": user.username, "cars": [{"make": car.make, "model": car.model, "year": car.year} for car in cars]}}), 200
+
+# Route for user registration
+@blueprint.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+         # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'msg': 'User created successfully'}), 201
+    elif request.method == 'POST':
+        return jsonify({'msg': 'Missing username or password'}), 400
+    return render_template('signup.html')
+
+
